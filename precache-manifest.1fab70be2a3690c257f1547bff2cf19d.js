ba86a9eb4bc871109e58ae5cb738c7938ec76a32
mandoopapa <remonytea@gmail.com>
2023-04-05 13:24:46 +0900

    Updates

---
+++ precache-manifest.1fab70be2a3690c257f1547bff2cf19d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64eb1ca191907f6da8b578d0a86baede",
+    "url": "/vdl-web/index.html"
+  },
+  {
+    "revision": "0dc14a7950f5903f100c",
+    "url": "/vdl-web/static/css/main.76e27cc9.chunk.css"
+  },
+  {
+    "revision": "d6bcd328fe15bac35d8c",
+    "url": "/vdl-web/static/js/2.95a8fa88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vdl-web/static/js/2.95a8fa88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dc14a7950f5903f100c",
+    "url": "/vdl-web/static/js/main.d3689e42.chunk.js"
+  },
+  {
+    "revision": "6ed15c596b04ce849c0b",
+    "url": "/vdl-web/static/js/runtime-main.db84b7e3.js"
+  },
+  {
+    "revision": "ff5c3f0d4af74967d1d257cabad72f98",
+    "url": "/vdl-web/static/media/logo.ff5c3f0d.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/vdl-web/precache-manifest.1fab70be2a3690c257f1547bff2cf19d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vdl-web/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
